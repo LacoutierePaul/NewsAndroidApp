890b7fef98a6e20f7d5281e470383b8440e657cd
LacoutierePaul <paul.lacoutiere@gmail.com>
2023-12-06 16:45:53 +0100

    Ajout Login and shared preferences

--- app/src/main/java/com/example/test_github/MainFragmen.kt
+++ app/src/main/java/com/example/test_github/MainFragmen.kt
@@ -1,5 +1,6 @@
 package com.example.test_github
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +21,26 @@ class MainFragment : Fragment() {
         view.findViewById<Button>(R.id.buttonNavigate).setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_recyclerViewFragment)
         }
+        val buttonLogout = view.findViewById<Button>(R.id.buttonLogout)
+        buttonLogout.setOnClickListener {
+            logout()
+        }
+
+
+
+
         return view
+    }
+
+    private fun logout() {
+        // Supprimer les informations d'identification de SharedPreferences
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.remove("username")
+        editor.remove("password")
+        editor.apply()
+
+        // Naviguer vers l'écran de connexion après la déconnexion
+        findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
     }
 }
