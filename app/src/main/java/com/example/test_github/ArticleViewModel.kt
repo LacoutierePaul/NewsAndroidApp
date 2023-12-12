77498852901673988937a61b98153e1423ed9f76
LacoutierePaul <paul.lacoutiere@gmail.com>
2023-12-12 21:18:10 +0100

    Deplacement apiInterface

--- app/src/main/java/com/example/test_github/ArticleViewModel.kt
+++ app/src/main/java/com/example/test_github/ArticleViewModel.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.test_github.Fragments.Apiinterface
 import com.example.test_github.Utils.ArticleDatabase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
